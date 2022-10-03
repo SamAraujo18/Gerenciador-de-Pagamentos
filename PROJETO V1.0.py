4f60c92c0c89d4f420961a4553cbf77b51ea6c9b
CrowZek <114800332+SamAraujo18@users.noreply.github.com>
2022-10-03 12:30:08 -0300

    Update PROJETO V1.0.py

--- PROJETO V1.0.py
+++ PROJETO V1.0.py
@@ -6,7 +6,7 @@ sleep(1.7)
 print(f'Olá,\033[31m {nome}\033[m seja bem vindo ao caixa eletrônico \033[36monline\033[m da loja')
 print('-=-' * 20)
 sleep(3)
-preco = float(input('Digite o preço do seu produto: '))
+preco = float(input('Digite o preço do seu produto: R$'))
 print('-=-' * 20)
 print("""Selecione abaixo a forma de pagamento:
 
@@ -25,6 +25,8 @@ if forma == '2':
 if forma == '3':
     print(f'O Valor do produto em até 2x no cartão vai ficar em torno de \033[1;92mR${preco:.2f}\033[m no valor total')
 if forma == '4':
-    print(f'O Valor do produto em até 3x ou mais no cartão vai ficar em torno de \033[1;92mR${preco + (preco * 20/100):.2f}\033[m')
+    parcela = int(input('Quantas parcelas? '))
+    print(f'Sua compra sera parcelada em até \033[1;97m{parcela}x\033[m de \033[1;92mR${parcela/100 * (preco + (preco * 20 / 100)):.2f}\033[m')
+    print(f'O produto vai custar \033[1;92mR${preco + (preco * 20 / 100):.2f}\033[m no valor final')
 if forma >= '5':
     print('FORMA DE PAGAMENTO INVÁLIDA!!')
